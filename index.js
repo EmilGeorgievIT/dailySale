3b22dc53238bef9118d995680886cf6a42f284fc
Emil <emil_georgiev_it@abv.bg>
2019-06-16 00:08:39 +0100

    Change server folder and add static folder for react

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const feedRoutes = require('./routes/feed');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const messageRoutes = require('./routes/message');
+const path = require('path');
 const { port } = require('./config/config');
-
 require('./database/database')();
 const app = express();
 
@@ -31,5 +31,13 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message });
   next();
 })
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(port, () => { console.log(`REST API listening on port: ${port}`) });
