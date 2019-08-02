079196958abe8f1107be255f9143e0e295839aed
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2019-08-02 20:23:42 +0100

    Fix favorite merge issues

--- routes/favorite.js
+++ routes/favorite.js
@@ -3,10 +3,6 @@ const favoriteController = require('../controllers/favorite');
 const isAuth = require('../middleware/is-auth');
 
 router.post('/addFavorite', isAuth, favoriteController.addFavorite);
-<<<<<<< HEAD
-// router.get('/getFavorite/:userId', favoriteController.getFavorites);
-=======
 router.get('/getFavorites/:userId', isAuth, favoriteController.getFavorites);
->>>>>>> 0b57163906377d8088075b82bcb3a1bda10b51fb
 
 module.exports = router;
